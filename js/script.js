b90b975b39578f2d28ad6e5520f2b4fe0b90d3fd
Otavio Augusto <hwe.otavio@hotmail.com>
2021-06-21 22:02:56 -0300

    update JS 01

---
+++ js/script.js
@@ -0,0 +1,6 @@
+// Obtendo todas as requisições dos elementos
+const start_btn = document.querySelector(".start_btn button");
+const info_caixa = document.querySelector(".info_caixa");
+const sair_btn = info_caixa.querySelector(".botoes .sair");
+const continue_btn = info_caixa.querySelector(".botoes .reiniciar");
+
