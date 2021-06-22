700ebb5cfc7e762773095f6a256191587e3136ac
Otavio Augusto <hwe.otavio@hotmail.com>
2021-06-22 15:50:41 -0300

    update JS 02

---
+++ js/questoes.js
@@ -0,0 +1,113 @@
+// Criando um Array e passando os numeros, questões e respostas
+let questions = [
+    {
+        numb: 1,
+        question: "SQL é a sigla para que palavras da língua inglesa?",
+        answer: "Structured Query Language",
+        options: [
+            "Structured Query Language",
+            "Stylesheet Query Language",
+            "Statement Question Language",
+            "Stylish Question Language"
+        ]
+    },
+    {
+        numb: 2,
+        question: "Ano de fundação do Google??",
+        answer: "1998",
+        options: [
+            "1989",
+            "1998",
+            "2004",
+            "2001"
+        ]
+    },
+    {
+        numb: 3,
+        question: "Qual a versão atual do HTML?",
+        answer: "5",
+        options: [
+            "5",
+            "PHP",
+            "2",
+            "Ultra"
+        ]
+    },
+    {
+        numb: 4,
+        question: "HTML é a sigla para que palavras da língua inglesa?",
+        answer: "HyperText Markup Language",
+        options: [
+            "HynerTarget Markup Layout",
+            "HyperText Much Large",
+            "HyperText Markup Language",
+            "HyperText Montup Layout"
+        ]
+    },
+    {
+        numb: 5,
+        question: "Qual o navegador mais utilizado atualmente?",
+        answer: "Google Chrome",
+        options: [
+            "Google Chrome",
+            "Microsoft Internet Explorer",
+            "Mozilla Firefox",
+            "Apple Safari"
+        ]
+    },
+    {
+        numb: 6,
+        question: "O que significa a sigla “www” na internet?",
+        answer: "World wide web",
+        options: [
+            "Web world wide",
+            "Web wide world",
+            "World wide web",
+            "Wide web world"
+        ]
+    },
+    {
+        numb: 7,
+        question: "Qual foi o primeiro tweet da história?",
+        answer: "Estou preparando meu twitter",
+        options: [
+            "Olá, Twitter!",
+            "Olá, mundo!",
+            "Estou preparando meu twitter",
+            "Pronto para dominar o mundo!"
+        ]
+    },
+    {
+        numb: 8,
+        question: "Em média, quantas pesquisas totalmente novas são feitas no Google por dia?",
+        answer: "450 milhões",
+        options: [
+            "1 bilhão",
+            "500 milhões",
+            "450 milhões",
+            "280 milhões"
+        ]
+    },
+    {
+        numb: 9,
+        question: "Qual foi a primeira rede social da história da internet?",
+        answer: "Classmate",
+        options: [
+            "MySpace",
+            "Classmate",
+            "Orkut",
+            "Facebook"
+        ]
+    },
+    {
+        numb: 10,
+        question: "Quantos bits cabem em um byte?",
+        answer: "8 bits",
+        options: [
+            "8 bits",
+            "1 bit",
+            "12 bits",
+            "16 bits"
+        ]
+    },
+];
--- js/script.js
+++ js/script.js
@@ -1,6 +1,32 @@
 // Obtendo todas as requisições dos elementos
 const start_btn = document.querySelector(".start_btn button");
 const info_caixa = document.querySelector(".info_caixa");
-const sair_btn = info_caixa.querySelector(".botoes .sair");
-const continue_btn = info_caixa.querySelector(".botoes .reiniciar");
+const sair = info_caixa.querySelector(".botoes .sair");
+const reiniciar = info_caixa.querySelector(".botoes .reiniciar");
+const caixa_quiz = document.querySelector(".caixa_quiz");
 
+
+ // Ação do botão Start
+ start_btn.onclick = ()=>{
+     info_caixa.classList.add("activeInfo"); // Mostrar a caixa de informação "Regras"
+ }
+
+// Ação do botão Sair
+sair.onclick = ()=>{
+    info_caixa.classList.remove("activeInfo"); // Fechar a caixa de informação "Regras"
+}
+
+// Ação do botão Continuar
+reiniciar.onclick = ()=>{
+    info_caixa.classList.remove("activeInfo"); // Fechar a caixa de informação "Regras"
+    caixa_quiz.classList.add("activeQuiz"); // Mostrar a caixa do Quiz
+    showQuestions()
+}
+
+let contQuestoes = 0;
+// Obtendo as questões e opções do Array
+function showQuestions(){
+    const questao_texto = document.querySelector(".questao_texto")
+    let que_tag = '<span>' + questions[].question + '</span>';
+    questao_texto.innerHTML = que_tag;
+}
