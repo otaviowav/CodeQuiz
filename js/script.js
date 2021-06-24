d29eb75ea55af4532cb1013ffa53dbbf457dad78
Otavio Augusto <hwe.otavio@hotmail.com>
2021-06-23 21:48:34 -0300

    update JS 04

--- js/script.js
+++ js/script.js
@@ -5,6 +5,8 @@ const sair = info_caixa.querySelector(".botoes .sair");
 const reiniciar = info_caixa.querySelector(".botoes .reiniciar");
 const caixa_quiz = document.querySelector(".caixa_quiz");
 const lista_de_opcao = document.querySelector(".lista_de_opcao")
+const timeCount = caixa_quiz.querySelector(".tempo .tempo_segundo");
+const linhaTempo = caixa_quiz.querySelector("header .linha_tempo");
 
 
 // Ação do botão Start
@@ -21,14 +23,24 @@ sair.onclick = () => {
 reiniciar.onclick = () => {
     info_caixa.classList.remove("activeInfo"); // Fechar a caixa de informação "Regras"
     caixa_quiz.classList.add("activeQuiz"); // Mostrar a caixa do Quiz
-    showQuestions(0)
-    questCont(1)
+    showQuestions(0);
+    questCont(1);
+    startTimer(15);
+    startTimerLine(0);
 }
 
 let contQuestoes = 0;
 let numQuestao = 1;
+let counter;
+let valorTempo = 15;
+let widthValue = 0;
+let userScore = 0;
+
 
 const proximo_btn = caixa_quiz.querySelector(".proximo_btn");
+const caixa_de_resultado = document.querySelector(".caixa_de_resultado");
+const reiniciar_quiz = caixa_de_resultado.querySelector(".botoes .reiniciar");
+const sair_quiz = caixa_de_resultado.querySelector(".botoes .sair");
 
 // Ação do botão "Proxima"
 proximo_btn.onclick = () => {
@@ -37,8 +49,14 @@ proximo_btn.onclick = () => {
         numQuestao++;
         showQuestions(contQuestoes);
         questCont(numQuestao);
+        clearInterval(counter)
+        startTimer(valorTempo);
+        clearInterval(counterLine)
+        startTimerLine(widthValue);
+        proximo_btn.style.display = "none";
     } else {
-        console.log("Quiz completado")
+        console.log("Quiz completado");
+        showResultBox();
     }
 }
 
@@ -64,10 +82,14 @@ let tickIcon = '<div class="icon tick"><i class="fas fa-check"></i></div>';
 let crossIcon = '<div class="icon cross"><i class="fas fa-times"></i></div>';
 
     function optionSelected(answer){
+        clearInterval(counter);
+        clearInterval(counterLine);
         let userResp = answer.textContent;
         let respCorreta = questions[contQuestoes].answer;
         let alloptions = lista_de_opcao.children.length;
         if(userResp == respCorreta){
+            userScore += 1;
+            console.log(userScore);
             answer.classList.add("correta");
             console.log("Resposta correta");
             answer.insertAdjacentHTML("beforeend", tickIcon);
@@ -90,7 +112,55 @@ let crossIcon = '<div class="icon cross"><i class="fas fa-times"></i></div>';
             lista_de_opcao.children[i].classList.add("desabilitado")
             
         }
+        proximo_btn.style.display = "block";
     }
+
+function showResultBox(){
+    info_caixa.classList.remove("activeInfo"); // Fechar a caixa de informação "Regras"
+    caixa_quiz.classList.remove("activeQuiz"); // Fechar a caixa do Quiz
+    caixa_de_resultado.classList.add("activeResult"); // Mostrar a caixa de resultado
+    const scoreText = caixa_de_resultado.querySelector(".score_do_teste");
+    if(userScore > 3){
+        let scoreTag = '<span>Parabéns, pequeno Padawan! você obteve <p>'+ userScore +'</p> de <p>'+ questions.length +'</p></span>';
+        scoreText.innerHTML = scoreTag;
+    }
+    else if(userScore > 1){
+        let scoreTag = '<span>Humm, legal mas você obteve apenas <p>'+ userScore +'</p> de <p>'+ questions.length +'</p></span>';
+        scoreText.innerHTML = scoreTag;
+    }
+    else{
+        let scoreTag = '<span>Sinto muito gafanhoto, você obteve apenas <p>'+ userScore +'</p> de <p>'+ questions.length +'</p></span>';
+        scoreText.innerHTML = scoreTag;
+    }
+}
+
+function startTimer(time){
+    counter = setInterval(timer, 1000);
+    function timer(){
+        timeCount.textContent = time;
+        time--;
+        if(time < 9){
+            let addZero = timeCount.textContent;
+            timeCount.textContent = "0" + addZero
+        }
+        if(time < 0){
+            clearInterval(counter)
+            timeCount.textContent = "00";
+        }
+    }
+}
+
+function startTimerLine(time){
+    counterLine = setInterval(timer, 29);
+    function timer(){
+        time += 1;
+        linhaTempo.style.width = time + "px";
+        if(time > 549){
+            clearInterval(counterLine);
+        }
+    }
+}
+
 
 
 function questCont(index) {
